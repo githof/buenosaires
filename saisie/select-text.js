d3b9ff43c75898f5fe9502779ccc50ef2d6784da
Christophe Prieur <prieur@liafa.fr>
2015-08-24 16:41:10 +0200

    SelectText + todo

--- saisie/select-text.js
+++ saisie/select-text.js
@@ -7,6 +7,28 @@ var text;
   cf. innerText vs. textContent
 */
 
+/*
+  From
+  https://stackoverflow.com/questions/985272/selecting-text-in-an-element-akin-to-highlighting-with-your-mouse/987376#987376
+*/
+function SelectText(element) {
+    var doc = document
+        , text = doc.getElementById(element)
+        , range, selection
+    ;    
+    if (doc.body.createTextRange) {
+        range = document.body.createTextRange();
+        range.moveToElementText(text);
+        range.select();
+    } else if (window.getSelection) {
+        selection = window.getSelection();        
+        range = document.createRange();
+        range.selectNodeContents(text);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
+}
+
 function selection()
 {
     var sel;
