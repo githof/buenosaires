125aa96c533befd799f3d477544176c39507e135
Christophe Prieur <prieur@liafa.fr>
2015-08-25 17:24:09 +0200

    create_elements, à la hache

--- saisie/taggable_element.js
+++ saisie/taggable_element.js
@@ -1,5 +1,39 @@
 
-function taggable_element(type, id)
+function create_elements()
 {
+    this.container.append(
+	$('<section>',
+	  {
+	      "class": "taggable_element",
+	      id: this.id
+	  }).append([
+		  $('<dl>',
+		    {
+			"class": "xml"
+		    }
+		   ).append([
+		       $('<dt>',
+		       {
+			   text: this.tag
+		       }),
+		       $('<dd>',
+		       {
+			   text: "test text"
+		       })
+		   ])
+	  ])
+    );
 }
 
+function taggable_element(id, tag, container)
+{
+    this.id = id;
+    this.tag = tag;
+    this.container = container;
+    this.create_elements = create_elements;
+    this.create_elements();
+}
+
+$(document).ready(function(){
+    $acte = new taggable_element("test_tag", "acte", $("#test"));
+});
