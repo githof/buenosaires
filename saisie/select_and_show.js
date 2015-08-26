04ca970e3f7ebfbdfc97da2de0f9ff0cf1e2ea35
Christophe Prieur <prieur@liafa.fr>
2015-08-26 18:02:54 +0200

    that

--- saisie/select_and_show.js
+++ saisie/select_and_show.js
@@ -19,25 +19,23 @@ function select_and_show($select, $show)
     var that = this;
     this.$select = $select;
     this.$show = $show;
-    this.bob = 3;
 
     this.show_selected = function ()
     {
 	var sel = selection();
-	this.$show.text(sel);
+	that.$show.text(sel);
     }
 
     this.stop_selection = function ()
     {
-	this.show_selected();
-	this.$select.off('mousemove mouseup');
+	that.show_selected();
+	that.$select.off('mousemove mouseup');
     }
 
     this.start_selection = function ()
     {
-	console.log(that.bob);
-	this.$select.on('mousemove', this.show_selected);
-	this.$select.on('mouseup', this.stop_selection);
+	that.$select.on('mousemove', that.show_selected);
+	that.$select.on('mouseup', that.stop_selection);
     }
 
     this.$select.on('mousedown', this.start_selection);
