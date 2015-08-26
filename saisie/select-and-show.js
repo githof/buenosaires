b72b455bdac61300b21c6118939c48a64efabd99
Christophe Prieur <prieur@liafa.fr>
2015-08-26 13:59:56 +0200

    start_selection

--- saisie/select-and-show.js
+++ saisie/select-and-show.js
@@ -3,6 +3,21 @@ function select_and_show($select, $show)
 {
     this.$select = $select;
     this.$show = $show;
-    
+
+    this.show_selected = function ()
+    {
+    }
+
+    this.stop_selection = function ()
+    {
+    }
+
+    this.start_selection = function ()
+    {
+	this.$select.on('mousemove', this.show_selected);
+	this.$select.on('mouseup', this.stop_selection);
+    }
+
+    this.$select.on('mousedown', this.start_selection);
 }
 
