e30acd206dcfa57a452d4903f445c9138e9d334d
Christophe Prieur <prieur@liafa.fr>
2015-08-27 22:18:29 +0200

    test avec création

--- saisie/test_select_and_show.js
+++ saisie/test_select_and_show.js
@@ -9,4 +9,32 @@ $(document).ready(function(){
 	    console.log('after: ' + this.after);
 	}
     );
+
+    var $source = $('<textarea>',
+		     {
+			 'class': "xml_text source",
+			 disabled: "true"
+		     });
+
+    var $show = $('<textarea>',
+		     {
+			 'class': "xml_text show",
+			 disabled: "true"
+		     });
+
+    $("body").append([ $source, $show ]);
+    $source.text("salut Tal, c'est encore moi !");
+
+    var S2 = new select_and_show(
+	$source,
+	$show,
+	function ()
+	{
+	    console.log('before: ' + this.before);
+	    console.log('selection: ' + this.select);
+	    console.log('after: ' + this.after);
+	}
+    );
+
+    
 });
