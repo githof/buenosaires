daf2d88b2c2c6b7f05b4409d1a1d9f1c786059ef
Christophe Prieur <prieur@liafa.fr>
2016-07-19 12:08:15 +0200

    parrains

--- sql/insert.sql
+++ sql/insert.sql
@@ -4,6 +4,7 @@ INSERT INTO statut (id, value) VALUES
     (2, 'epouse'),
     (3, 'pere'),
     (4, 'mere'),
-    (5, 'temoin');
+    (5, 'temoin'),
+    (6, 'parrain');
 
 INSERT INTO source (id, value) VALUES (1, 'Matrimonios');
