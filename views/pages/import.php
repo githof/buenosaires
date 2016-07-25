cdf1ef9112ecfff1d69fa392e18bfd28f22021df
Cedeify <cedeify@hotmail.fr>
2016-07-25 13:27:45 +0200

    handle errors import actes

--- views/pages/import.php
+++ views/pages/import.php
@@ -2,28 +2,90 @@
 
     include_once(ROOT."src/XMLActeReader.php");
 
+    function error_message_receive_file($error_code){
+        $log = "Erreur ($error_code) inconnue lors du téléchargement du fichier";
+        $alert = "Erreur lors du téléchargement du fichier vers le serveur";
+
+        switch($error_code){
+            case 1:
+                $message = "La taille du fichier téléchargé excède la valeur maximale supportée par le serveur";
+                $log = $message;
+                $alert = $message;
+                break;
+            case 2:
+                $message = "La taille du dichier téléchargé excède la valeur MAX_FILE_SIZE, qui a été spécifiée dans le formulaire HTML";
+                $log = $message;
+                $alert = $message;
+                break;
+            case 3:
+                $message = "Le fichier n'a été que partiellement téléchargé";
+                $log = $message;
+                $alert = $message;
+                break;
+            case 4:
+                $message = "Aucun fichier n'a été téléchargé";
+                $log = $message;
+                $alert = $message;
+                break;
+            case 6:
+                $log = "Un fichier temporaire est manquant";
+                break;
+            case 7:
+                $log = "Echec de l'écriture du fichier sur le disque";
+                break;
+            case 8:
+                $log = "Une extension PHP a arrêté l'envoi de fichier";
+                break;
+        }
+        return [
+            "log" => $log,
+            "alert" => $alert
+        ];
+    }
 
     function receive_file(){
-        global $alert;
+        global $alert, $log;
+
+        if($_FILES["import_file"]["error"] > 0){
+            $tabs = error_message_receive_file($_FILES["import_file"]["error"]);
+            $log->e($tabs["log"]);
+            $alert->error($tabs["alert"]);
+            return NULL;
+        }
 
         $infos = pathinfo($_FILES["import_file"]["name"]);
         if($infos["extension"] === "xml"){
             $uploadfile = TMP_DIRECTORY . "/" . basename($_FILES["import_file"]["name"]);
-            move_uploaded_file($_FILES["import_file"]["tmp_name"], $uploadfile);
-	    // TODO : test erreur (la fonction renvoie FALSE)
+            if(@move_uploaded_file($_FILES["import_file"]["tmp_name"], $uploadfile) === FALSE){
+                $log->e("Erreur fct move_uploaded_file({$_FILES['import_file']['tmp_name']}, $uploadfile)");
+                $alert->error("Erreur interne du serveur lors du téléchargement du fichier");
+                return NULL;
+            }
             return $uploadfile;
         }else{
             $alert->error("Le fichier doit être au format XML");
+            return NULL;
         }
     }
 
     function receive_text(){
+        global $alert, $log;
+
         $sources = '<?xml version="1.0" encoding="UTF-8"?>';
         $sources = "<document><ACTES>\n".stripslashes($_POST['import_text'])."\n</ACTES></document>";
 
         $filename = TMP_DIRECTORY . "/new_actes.xml";
-        $tmp_file = fopen($filename, "w");
-        fwrite($tmp_file, $sources);
+        $tmp_file = @fopen($filename, "w");
+        if($tmp_file === FALSE){
+            $log->e("Erreur de la fct fopen($filename, 'w')");
+            $alert->error("Erreur interne du serveur lors de l'import");
+            return NULL;
+        }
+        if(fwrite($tmp_file, $sources) === FALSE){
+            $log->e("Erreur de la fct fwrite($tmp_file, $sources)");
+            $alert->error("Erreur interne du serveur lors de l'import");
+            return NULL;
+        }
         return $filename;
     }
 
@@ -60,9 +122,11 @@
             $source_id = $_POST["import_text_source"];
         }
 
-        $reader = new XMLActeReader($source_id);
-        $reader->use_xml_file($filename);
-        $reader->read_actes($only_new);
+        if($filename != NULL){
+            $reader = new XMLActeReader($source_id);
+            $reader->use_xml_file($filename);
+            $reader->read_actes($only_new);
+        }
     }
 
 ?>
