1629c08ddc1d6fc4047307c9d56268c76f7e375a
Christophe Prieur <prieur@liafa.fr>
2016-07-27 19:30:32 +0200

    get_ids

--- bricolages/session.sh
+++ bricolages/session.sh
@@ -1,7 +1,14 @@
 
 xml_belgrano=DATA/grep-belgrano.xml
 
-cat $xml_belgrano \
-    | sed -n 's#id="\([0-9]*\)"#\1@#gp' \
-    | tr '@' '\n'
+get_ids ()
+{
+    f="$1"
+    cat $f \
+	| sed 's#id="\([0-9]*\)".*#@\1@#gp' \
+	| tr '@' '\n' \
+	| grep '^[0-9][0-9]*$' \
+	| sort -u
+}
+get_ids $xml_belgrano
 
