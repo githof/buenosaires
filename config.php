af1e696b18b26e8aa9250411076a4f216a1ce11c
Christophe Prieur <prieur@liafa.fr>
2016-09-06 12:13:37 +0200

    local-config

--- config.php
+++ config.php
@@ -1,21 +1,6 @@
 <?php
 
-// base URL for all relative URLs
-// define("BASE_URL", "https://buenosaires.telecom-paristech.fr/");
-
-define("BASE_URL", "http://localhost/buenosaires/");
-
-
-//  Server MYSQL
-// define("SQL_SERVER", "mysql-projet.enst.fr");
-// define("SQL_DATABASE_NAME", "buenosaires_TPT");
-// define("SQL_USER", "buenosaires.TPT");
-// define("SQL_PASS", "UvPsQ8RrB3OuDApImf5Y");
-
-define("SQL_SERVER", "localhost");
-define("SQL_DATABASE_NAME", "buenosaires_TPT");
-define("SQL_USER", "root");
-define("SQL_PASS", "root");
+include('local-config.php');
 
 //  log default output file
 define("LOG_DEFAULT_OUTPUT", "log.txt");
