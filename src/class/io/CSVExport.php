13a08b47d6b479eca9b7e257de31afd4369cee7c
cedeify <ceddesgranges@gmail.com>
2016-09-20 17:30:47 +0200

    csv all personnes

---
+++ src/class/io/CSVExport.php
@@ -0,0 +1,64 @@
+<?php
+
+    function array_to_string($array, $separator){
+        $str = "";
+        $i = 0;
+        $length = count($array);
+        foreach($array as $item){
+            $str .= $item;
+            if($i < $length -1)
+                $str .= $separator;
+            $i++;
+        }
+        return $str;
+    }
+
+    class CSVExport {
+
+        var $CSV_SEPARATOR = ",";
+
+        function __construct(){
+
+        }
+
+        function export_personnes(){
+            global $mysqli;
+
+            $this->entete();
+
+            echo "id".$this->CSV_SEPARATOR."noms".$this->CSV_SEPARATOR."prenoms".PHP_EOL;
+
+            $results = $mysqli->select("personne", ["id"]);
+            if($results != FALSE && $results->num_rows){
+                while($row = $results->fetch_assoc()){
+                    $personne = new Personne($row["id"]);
+                    $mysqli->from_db($personne);
+
+                    $prenoms = [];
+                    foreach($personne->prenoms as $prenom)
+                        $prenoms[] = $prenom->to_string();
+
+                    $noms = [];
+                    foreach($personne->noms as $nom)
+                        $noms[] = $nom->to_string();
+
+                    $prenoms = array_to_string(
+                        $prenoms,
+                        " ");
+                    $noms = array_to_string(
+                        $noms,
+                        " ");
+
+                    echo "$personne->id".$this->CSV_SEPARATOR."$noms".$this->CSV_SEPARATOR."$prenoms".PHP_EOL;
+                }
+            }
+        }
+
+        function entete(){
+            header('Content-type: text/csv');
+            header('Content-Disposition: attachment; filename="export.csv"');
+        }
+    }
+
+
+?>
--- src/views/pages/export.php
+++ src/views/pages/export.php
@@ -1,22 +1,46 @@
 <?php
 
     include_once(ROOT."src/class/io/XMLExport.php");
+    include_once(ROOT."src/class/io/CSVExport.php");
 
     if(isset($ARGS["export"])){
         switch($ARGS["what"]){
             case "all_actes":
-                $export = new XMLExport();
-                $export->export_all();
+                if($ARGS["export"] == "xml"){
+                    $export = new XMLExport();
+                    $export->export_all();
+                }
                 break;
-
+            case "all_personnes":
+                if($ARGS["export"] == "csv"){
+                    $export = new CSVExport();
+                    $export->export_personnes();
+                }
+                break;
         }
     }else{
 ?>
 <section>
     <h4>Tous les actes</h4>
     <div>
-        <a class="btn btn-info btn-sm" href="export?export=xml&what=all_actes">
+        <a class="btn btn-info btn-sm bold" href="export?export=xml&what=all_actes">
             XML
+        </a>
+    </div>
+</section>
+<section>
+    <h4>Toutes les personnes</h4>
+    <div>
+        <a class="btn btn-info btn-sm bold" href="export?export=csv&what=all_personnes">
+            CSV
+        </a>
+    </div>
+</section>
+<section>
+    <h4>Toutes les relations</h4>
+    <div>
+        <a class="btn btn-info btn-sm bold" href="export?export=csv&what=all_relations">
+            CSV
         </a>
     </div>
 </section>
