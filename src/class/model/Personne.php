49292e98d63e3899dab3b08b55dfcccbbcc1d836
cedeify <ceddesgranges@gmail.com>
2016-09-23 15:13:25 +0200

    change how personne is insert into db

--- src/class/model/Personne.php
+++ src/class/model/Personne.php
@@ -15,10 +15,9 @@
         var $relations;
         var $conditions;
 
-        var $pere;
-        var $mere;
-
         var $xml;
+
+        var $is_updated_in_db;
 
         function __construct($id = NULL){
             $this->id = $id;
@@ -26,28 +25,31 @@
             $this->noms = [];
             $this->conditions = [];
             $this->relations = [];
-            $this->pere = NULL;
-            $this->mere = NULL;
+            $this->is_updated_in_db = FALSE;
         }
 
         public function add_prenom($prenom){
             foreach($this->prenoms as $_prenom){
-                if((isset($_prenom->id, $prenom->id) && $_prenom->id == $prenom->id) || $_prenom->no_accent == $prenom->no_accent)
+                if((isset($_prenom->id, $prenom->id)
+                        && $_prenom->id == $prenom->id)
+                    || $_prenom->no_accent == $prenom->no_accent)
                     return;
             }
             $this->prenoms[] = $prenom;
         }
 
-        public function add_nom($nom, $ordre = NULL){
+        public function add_nom($nom){
             foreach($this->noms as $_nom){
-                if((isset($_nom->id, $nom->id) && $_nom->id == $nom->id) ||
-                    ($_nom->no_accent == $nom->no_accent))
-                    return;
+                if((isset($_nom->id, $nom->id)
+                        && $_nom->id == $nom->id)
+                    || ($_nom->no_accent == $nom->no_accent)){
+                        if(isset($nom->attribut)){
+                            $_nom->attribut = $nom->attribut;
+                        }
+                        return;
+                    }
             }
-            if(isset($ordre))
-                array_splice($this->noms, $ordre, 0, $nom);
-            else
-                $this->noms[] = $nom;
+            this->noms[] = $nom;
         }
 
         public function add_condition($text, $source_id){
@@ -64,11 +66,11 @@
         }
 
         public function set_pere($pere){
-            $this->pere = $pere;
+            $this->add_relation($pere, $this, STATUT_PERE);
         }
 
         public function set_mere($mere){
-            $this->mere = $mere;
+            $this->add_relation($mere, $this, STATUT_MERE);
         }
 
         public function set_xml($xml){
@@ -105,16 +107,6 @@
             if(!$this->is_valid())
                 return FALSE;
 
-            if(isset($this->pere) && $this->pere->is_valid()){
-                $mysqli->into_db($this->pere);
-                $this->add_relation($this->pere, $this, STATUT_PERE);
-            }
-
-            if(isset($this->mere) && $this->mere->is_valid()){
-                $mysqli->into_db($this->mere);
-                $this->add_relation($this->mere, $this, STATUT_MERE);
-            }
-
             foreach($this->prenoms as $prenom){
                 $mysqli->into_db($prenom);
             }
@@ -129,6 +121,8 @@
         public function post_into_db(){
             global $mysqli;
 
+            $this->is_updated_in_db = TRUE;
+
             $mysqli->start_transaction();
             $i = 1;
             $mysqli->delete("prenom_personne", "personne_id='$this->id'");
@@ -137,6 +131,7 @@
                 $i++;
             }
 
+            $mysqli->delete("nom_personne", "personne_id='$this->id'");
             $i = 1;
             foreach($this->noms as $nom){
                 $mysqli->into_db_nom_personne($this, $nom, $i);
