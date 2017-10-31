b350b3fbcb96539d0bb7a8508e10eac16873e682
Christophe Prieur <prieur@liafa.fr>
2017-10-31 12:42:39 +0100

    add header+footer to chunks

--- admin/xml/split.sh
+++ admin/xml/split.sh
@@ -6,28 +6,41 @@ lines=$2
 header=DATA/header.xml
 footer=DATA/footer.xml
 
-if ! test -r "$header" -o ! test -r "$footer"
-then
-    (
-	echo "erreur"
-	echo "j'ai besoin de $header et $footer"
-	echo "qui sont sans doute ceci :"
-	echo "head -1 $1"
-	head -1 $1
-	echo "tail -1 $1"
-	tail -1 $1
-    ) >&2
-    exit
-fi
+check-header-footer ()
+{
+    if test ! \( -r "$header" -a -r "$footer" \)
+    then
+	(
+	    echo "erreur"
+	    echo "j'ai besoin de $header et $footer"
+	    echo "qui sont sans doute ceci :"
+	    echo "head -1 $1"
+	    head -1 $1
+	    echo "tail -1 $1"
+	    tail -1 $1
+	) >&2
+	exit
+    fi
+}
+check-header-footer
 
-cat $xml \
-    | tail -n +2 \
-    | ghead -n -1 \
-    | split -a 5 -l $lines - "$xml"
+split-body ()
+{
+    cat $xml \
+	| tail -n +2 \
+	| ghead -n -1 \
+	| split -a 5 -l $lines - "$xml"
+}
+split-body
 
-for f in "$xml"?????
-do
-    new=`echo $f | sed 's#.xml\(.....\)$#-\1.xml`
-    mv $f $new
-done
+chunks ()
+{
+    for f in "$xml"?????
+    do
+	new=`echo $f | sed 's#.xml\(.....\)$#-\1.xml#'`
+	cat $header $f $footer > $new
+	rm $f
+    done
+}
+chunks
 
