00f3bd0b68b8a861744cded99bd10c891ef59be7
Christophe Prieur <cprieur@enst.fr>
2018-10-22 10:09:30 +0200

    DATA

--- admin/session.sh
+++ admin/session.sh
@@ -1,10 +1,15 @@
+#!/bin/bash
+
+DATA=DATA
 
 diff_versions ()
 {
     cd $DATA
     cat matrimonios-before-2016.xml \
 	| tr -s ' ' \
+	| sed 's#attr="de la"#de="true" la="true"#'
 	| diff - export-2018.xml \
 	       >| diff.xml
     cat diff.xml | sed -n 's#^.*<ACTE id="\([0-9]*\)">.*$#\1#p' | uniq | wc -l
 }
+diff_versions
