c4bd7976d4f55b6ee0aceeb7b03e46575e712d76
Christophe Prieur <cprieur@enst.fr>
2018-10-22 10:45:09 +0200

    session diff ok

--- admin/session.sh
+++ admin/session.sh
@@ -2,14 +2,48 @@
 
 DATA=DATA
 
+no_blank_lines ()
+{
+    grep -v '^>* *$'
+}
+
+adjust_before ()
+{
+    tr -s ' ' \
+       | no_blank_lines \
+       | sed 's# de="true" la="true"# attr="de la"#g' \
+       | sed 's# de="true"# attr="de"#g' \
+       | sed 's# y="true"# attr="y"#g'
+}
+
+by_words ()
+{
+    tr ' ' '\n' \
+       | grep -v '^ *$'
+}
+
 diff_versions ()
 {
     cd $DATA
-    cat matrimonios-before-2016.xml \
-	| tr -s ' ' \
-	| sed 's#attr="de la"#de="true" la="true"#'
-	| diff - export-2018.xml \
+    before=matrimonios-before-2016.xml
+    now=export-2018.xml
+    cat $before \
+	| adjust_before \
+	| diff - $now \
+	| no_blank_lines \
 	       >| diff.xml
-    cat diff.xml | sed -n 's#^.*<ACTE id="\([0-9]*\)">.*$#\1#p' | uniq | wc -l
+    cat diff.xml \
+	| sed -n 's#^.*<ACTE id="\([0-9]*\)">.*$#\1#p' \
+	| uniq \
+	| wc -l
+    cat $now \
+	| by_words \
+	     > now-words.txt
+
+    cat $before \
+	| adjust_before \
+	| by_words \
+	| diff - now-words.txt \
+	       >| diff-words.txt    
 }
 diff_versions
