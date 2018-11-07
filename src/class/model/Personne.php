91717763f941adbff75029ea62931fe3aa42c9b0
Christophe Prieur <cprieur@enst.fr>
2018-11-07 11:58:06 +0100

    autre tentative par error_log

--- src/class/model/Personne.php
+++ src/class/model/Personne.php
@@ -49,7 +49,8 @@
             $this->prenoms[] = $prenom;
 	    $str = $this->prenoms_str;
 	    $this->prenoms_str = ($str == "" ? "" : " ") . $prenom->to_string();
-	    syslog(LOG_DEBUG, "add_prenom $prenom -> $this->prenoms_str");
+	    error_log("add_prenom $prenom -> $this->prenoms_str");
+	    //	    syslog(LOG_DEBUG, "add_prenom $prenom -> $this->prenoms_str");
         }
 
         public function add_nom($nom){
