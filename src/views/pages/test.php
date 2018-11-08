9a47487a66804835e0e4c8890622673f17b7d440
Christophe Prieur <cprieur@enst.fr>
2018-11-08 18:04:58 +0100

    new Prenom

--- src/views/pages/test.php
+++ src/views/pages/test.php
@@ -6,7 +6,8 @@ include_once(ROOT."src/class/model/Personne.php");
 include_once(ROOT."src/html_entities.php");
 
 $personne = new Personne(1);
-$personne->add_prenom("Bob");
+$prenom = new Prenom(10000, "Bob", "Bob");
+$personne->add_prenom($prenom);
 echo "<p>";
 echo "to_string = $personne->prenoms[0]<br>";
 echo "_str = $personne->prenoms_str<br>";
