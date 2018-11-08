96d8860bc8e6a50adcec8940c6920f7ca2d2e5e0
Christophe Prieur <cprieur@enst.fr>
2018-11-08 18:07:33 +0100

    fix: to_string

--- src/views/pages/test.php
+++ src/views/pages/test.php
@@ -9,7 +9,7 @@ $personne = new Personne(1);
 $prenom = new Prenom(10000, "Bob", "Bob");
 $personne->add_prenom($prenom);
 echo "<p>";
-$p0 = $personne->prenoms[0];
+$p0 = $personne->prenoms[0]->to_string();
 echo "to_string = $p0<br>";
 echo "_str = $personne->prenoms_str<br>";
 echo "</p>";
