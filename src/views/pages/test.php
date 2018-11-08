17fb1663dcb28d60cf1c77ed900559bd23a27f4d
Christophe Prieur <cprieur@enst.fr>
2018-11-08 18:02:30 +0100

    fix

--- src/views/pages/test.php
+++ src/views/pages/test.php
@@ -8,7 +8,7 @@ include_once(ROOT."src/html_entities.php");
 $personne = new Personne(1);
 $personne->add_prenom("Bob");
 echo "<p>";
-echo "to_string = $personne->prenoms->to_string()<br>";
+echo "to_string = $personne->prenoms[0]<br>";
 echo "_str = $personne->prenoms_str<br>";
 echo "</p>";
 
