069cab632067dbc368e3af8103f11e18d9946df6
Christophe Prieur <cprieur@enst.fr>
2018-11-08 17:56:48 +0100

    test.php

---
+++ src/views/pages/test.php
@@ -0,0 +1,15 @@
+<?
+
+echo "hello, world";
+
+include_once(ROOT."src/class/model/Personne.php");
+include_once(ROOT."src/html_entities.php");
+
+$personne = new Personne(1);
+$personne->add_prenom("Bob");
+echo "<p>";
+echo "to_string = $personne->prenoms->to_string()<br>";
+echo "_str = $personne->prenoms_str<br>";
+echo "</p>";
+
+?>
