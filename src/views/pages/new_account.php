95bb39b0ad4875ac0d03aba6f1397897bf6eb0d9
morganprieur <morgan@chatnoir.lautre.net>
2021-03-02 12:11:49 +0100

    fonctions sur new_account (en cours)

--- src/views/pages/new_account.php
+++ src/views/pages/new_account.php
@@ -31,49 +31,64 @@ function check_post_values(){
     return !$email_error && !$password_error && !$prenom_error && !$nom_error;
 }
 
-
-function html_form_label($for, $label) {
-    return '<label class="col-sm-3 control-label" for="'.$for.'">'.$label.'</label>';
-}
-
 //  inputs ont une classe "has-error" si un champ n'est pas rempli correctement
-function html_input_if($email_error, $password_error, $prenom_error, $nom_error, $type, $name) {
+function html_input_if($label, $type, $name) {
+    global $email_error, $password_error, $prenom_error, $nom_error;
 
-    $email_error;
-    $password_error;
-    $prenom_error;
-    $nom_error;
-
-    // $form_error;
-
+    // $form_error = $email_error || $password_error || $prenom_error || $nom_error;
+    //  pour inputs
     $type;
     $name;
-    
     $class;
     $value;
 
     /*  
         La valeur de 'password' n'est pas conservée si le form contient une/des erreurs
-        Pour les autres champs, afficher de nouveau la valeur tapée
+        Pour les autres champs, afficher de nouveau la valeur saisie
     */
-    $name === 'password' ? $value = '' : $value =  'value="'.get_post_var($name).'"';
+    $value = $name === 'password' ? '' : 'value="'.get_post_var($name).'"';
 
     /*
-        Il faut récupérer les variables "true" de check_post_values() pour mettre la class "has-error" à l'input
-        (même si la class n'active pas la règle Bootstrap associée, avant même mes changements)
+    Il faut récupérer les variables "true" de check_post_values() pour mettre la class "has-error" à l'input
+    (même si la class n'active pas la règle Bootstrap associée, avant même mes changements. Je n'ai pas trouvé pourquoi)
     */  
+    //  $form_error renvoie false, pas des noms de variables 
     // ($form_error == $name.'_error') ? $class='form-control has-error' : $class='form-control';
-
-    return
-        '<div class="col-sm-9">
-            <input class="'.$class.'" type="'.$type.'" name="'.$name.'" id="'.$name.'" '.$value.'/>
+    if($name === 'email') { 
+        if($email_error===true) 
+            $class = ' has-error';
+        else 
+            $class = '';
+    } 
+    if($name === 'password') { 
+        if($password_error===true) 
+            $class = ' has-error';
+        else 
+            $class = '';
+    } 
+    if($name === 'prenom') { 
+        if($prenom_error===true) 
+            $class = ' has-error';
+        else 
+            $class = '';
+    } 
+    if($name === 'nom') { 
+        if($nom_error===true) 
+            $class = ' has-error';
+        else 
+            $class = '';
+    } 
+    $html = '<label class="col-sm-3 control-label" for="'.$name.'">'.$label.'</label>
+        <div class="col-sm-9">
+            <input class="form-control '.$class.'" type="'.$type.'" name="'.$name.'" id="'.$name.'" '.$value.'/>
         </div>';
+
+    return $html;
 }
 
-function html_form_group_if($for, $label, $email_error, $password_error, $prenom_error, $nom_error, $type, $name) {
+function html_form_group_if($label, $type, $name) {
     return '<div class="form-group">'.
-        html_form_label($for, $label).
-        html_input_if($email_error, $password_error, $prenom_error, $nom_error, $type, $name).'
+        html_input_if($label, $type, $name).'
     </div>';
 }
 
@@ -113,38 +128,49 @@ if($account->is_connected){
 
 <div id="form_new_account">
     <form class="form-horizontal" name="new_account" action="./new-account" method="post">
-    <?php 
-        // echo 
-        //     html_form_group_if('email', 'Email', $email_error, $password_error, $prenom_error, $nom_error, 'email', 'email').
-        //     html_form_group_if('password', 'Mot de passe', $email_error, $password_error, $prenom_error, $nom_error, 'password', 'password').
-        //     html_form_group_if('prenom', 'Prenom', $email_error, $password_error, $prenom_error, $nom_error, 'text', 'prenom').
-        //     html_form_group_if('nom', 'Nom', $email_error, $password_error, $prenom_error, $nom_error, 'text', 'nom');
-
-    ?>
         <!-- Les classes "has-error" n'activent pas la règle (border-color) de Bootstrap --> 
+        <?php 
+        // echo html_form_group_if($label, $type, $name);
+        ?>
         <div class="form-group">
-            <label class="col-sm-3 control-label" for="email">Email</label>
+            <?php 
+            echo 
+                html_input_if('Email', 'email', 'email');
+            ?>
+            <!-- <label class="col-sm-3 control-label" for="email">Email</label>
             <div class="col-sm-9">
-                <input class="form-control <?php  if($email_error) echo 'has-error' ?>" type="email" name="email" id="email" value="<?php echo get_post_var('email'); ?>" />
-            </div>
+                <input class="form-control <?php // if($email_error) echo 'has-error' ?>" type="email" name="email" id="email" value="<?php echo get_post_var('email'); ?>" />
+            </div> -->
         </div>
         <div class="form-group">
-            <label class="col-sm-3 control-label" for="password">Mot de passe</label>
+            <?php 
+            echo 
+                html_input_if('Password', 'password', 'password');
+            ?>
+            <!-- <label class="col-sm-3 control-label" for="password">Mot de passe</label>
             <div class="col-sm-9">
-                <input class="form-control <?php  if($password_error) echo 'has-error' ?>" type="password" name="password" id="password" />
-            </div>
+                <input class="form-control <?php // if($password_error) echo 'has-error' ?>" type="password" name="password" id="password" />
+            </div> -->
         </div>
         <div class="form-group">
-            <label class="col-sm-3 control-label" for="prenom">Prenom</label>
+            <?php 
+            echo 
+                html_input_if('Prenom', 'text', 'prenom');
+            ?>
+            <!-- <label class="col-sm-3 control-label" for="prenom">Prenom</label>
             <div class="col-sm-9">
-                <input class="form-control <?php  if($prenom_error) echo 'has-error' ?>" type="text" name="prenom" id="prenom" value="<?php echo get_post_var('prenom'); ?>" />
-            </div>
+                <input class="form-control <?php // if($prenom_error) echo 'has-error' ?>" type="text" name="prenom" id="prenom" value="<?php echo get_post_var('prenom'); ?>" />
+            </div> -->
         </div>
         <div class="form-group">
-            <label class="col-sm-3 control-label" for="nom">Nom</label>
+            <?php 
+            echo 
+                html_input_if('Nom', 'text', 'nom');
+            ?>
+            <!-- <label class="col-sm-3 control-label" for="nom">Nom</label>
             <div class="col-sm-9">
-                <input class="form-control <?php  if($nom_error) echo 'has-error' ?>" type="text" name="nom" id="nom" value="<?php echo get_post_var('nom'); ?>" />
-            </div>
+                <input class="form-control <?php // if($nom_error) echo 'has-error' ?>" type="text" name="nom" id="nom" value="<?php echo get_post_var('nom'); ?>" />
+            </div> -->
         </div>
         <div class="form-group">
             <?php
