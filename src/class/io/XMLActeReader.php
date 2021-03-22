1f5dd23fdec5620e7770a7a25b3fb36eddf10712
morganprieur <morgan@chatnoir.lautre.net>
2021-03-22 11:39:05 +0100

    Revert "fix set_personne_attributes (Don manquants) sur rewrite-set-p-attr"
    This reverts commit 64fc8dbba9d970a631387856a5aede4260afe773.

--- src/class/io/XMLActeReader.php
+++ src/class/io/XMLActeReader.php
@@ -179,12 +179,13 @@ class XMLActeReader {
 
     //  PRIVATE METHODS //
 
-    //  à corriger
     private function set_personne_attributes($p, $p_attr) {
+        if(isset($pers_attr["id"]))
+            $personne->id = $p_attr()["id"];
 
-        if(isset($p_attr["don"])
+        if(isset($pers_attr["don"])
             && $p_attr["don"] == "true")
-            $p->add_condition("Don", $this->source_id);
+            $personne->add_condition("Don", $this->source_id);
     }
 
     private function read_personne_child_node($personne, $xml_child) {
