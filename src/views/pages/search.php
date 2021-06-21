cf717b0899964ddbc6a872b3a37239c3e1a60502
morganprieur <morgan@chatnoir.lautre.net>
2021-06-21 11:24:56 +0200

    Factorisé fonctions sélect letters

--- src/views/pages/search.php
+++ src/views/pages/search.php
@@ -15,21 +15,22 @@ include_once(ROOT."src/html_entities.php");
 //     return $str;
 // }
 
+
 //  *** ex function all_noms() modifiée et divisée 
 /*** Il faut que l'utilisateur choisisse bien l'onglet qu'il veut 
  * pour sélectionner la/les lettre/s 
- * Si on choisit une letttre dans "actes", on n'a pas de prénoms sélectionnés dans "personnes". 
+ * Si on choisit une letttre dans "actes", on n'a pas de prénoms sélectionnés pour "personnes". 
  */ 
-  
-function selected_noms($nom_ou_prenom){ // 'nom' 
+function selected($nom_ou_prenom){
     global $mysqli;
     $str = "";
+    $name_select = "letter_$nom_ou_prenom";
 
-    if(isset($_GET['letter_nom'])) {
-        $letter = $_GET['letter_nom'];
+    if(isset($_GET[$name_select])) {
+        $letter = $_GET[$name_select];
 
-        // echo '<br> $letter_nom : ';
-        // var_dump($letter);
+        echo '<br>'. __METHOD__.' $letter : ';
+        var_dump($letter);
 
         if(($letter == 'pas de nom') || (empty($letter))) {
             $result = $mysqli->query("
@@ -54,41 +55,7 @@ function selected_noms($nom_ou_prenom){ // 'nom'
         }
     }
     return $str;
-}
-function selected_prenoms($nom_ou_prenom){  //  'prenom' '
-  global $mysqli;
-  $str = "";
-
-  if(isset($_GET['letter_prenom'])) {
-      $letter = $_GET['letter_prenom'];
-
-      // echo '<br> $letter_prenom : ';
-      // var_dump($letter);
-
-      if($letter == 'pas de nom') {
-          $result = $mysqli->query("
-          SELECT id, no_accent
-          FROM $nom_ou_prenom
-          ORDER BY no_accent
-          ");
-      } else {
-          //  lettre
-          $result = $mysqli->query("
-          SELECT id, no_accent
-          FROM  $nom_ou_prenom
-          WHERE $nom_ou_prenom LIKE '$letter%'
-          ORDER BY no_accent
-          ");
-      } 
-
-      if($result != FALSE && $result->num_rows > 0){
-          while($row = $result->fetch_assoc()){
-              $str .= "<option value='{$row["id"]}'>{$row["no_accent"]}</option>";
-          }
-      }
-  }
-  return $str;
-}
+} 
 
 function html_input_date($label, $name){
     return html_form_group ('
@@ -97,27 +64,14 @@ function html_input_date($label, $name){
     ');
 }
 
-//  $nom_ou_prenom = 'nom' 
-function html_select_personnes_nom($title, $name_select, $nom_ou_prenom) {
-    $html_option_list = selected_noms($nom_ou_prenom);
+function html_select_personnes($title, $name_select, $nom_ou_prenom) {
 
     return html_form_group('
         <label for="'.$name_select.'">'.$title.'</label>
         <select multiple="multiple" name="'.$name_select.'[]" id="'.$name_select.'">'
-            .$html_option_list.
+            .selected($nom_ou_prenom).
         '</select>
     ');
-}
-//  $nom_ou_prenom = 'prenom' 
-function html_select_personnes_prenom($title, $name_select, $nom_ou_prenom) {
-  $html_option_list = selected_prenoms($nom_ou_prenom);
-
-  return html_form_group('
-      <label for="'.$name_select.'">'.$title.'</label>
-      <select multiple="multiple" name="'.$name_select.'[]" id="'.$name_select.'">'
-          .$html_option_list.
-      '</select>
-  ');
 }
 
 function html_tab_titles(){
@@ -137,27 +91,17 @@ function html_search_acte()
 {
     return html_input_date('A partir de : ', 'acte_date_start')
         . html_input_date('Avant : ', 'acte_date_end')
-        . html_select_personnes_nom(
+        . html_select_personnes(
             'Contenant les personnes avec pour nom de famille', 
             'acte_noms', 
             'nom'
         );
 }
 
-function html_search_personne()
-{
-    // $contents = '';
-
-    // $contents .= html_select_personnes('Avec pour nom(s) de famille',
-    //     'personne_noms', 'nom');
-    // $contents .= html_select_personnes('Avec pour prénom(s)',
-    //     'personne_prenoms', 'prenom');
-
-    // return $contents;
-
-    return html_select_personnes_nom('Avec pour nom(s) de famille',
+function html_search_personne() {
+    return html_select_personnes('Avec pour nom(s) de famille',
             'personne_noms', 'nom') 
-          . html_select_personnes_prenom('Avec pour prénom(s)',
+          . html_select_personnes('Avec pour prénom(s)',
           'personne_prenoms', 'prenom');
 }
 
@@ -230,7 +174,7 @@ function html_tabpanel($class, $name, $acte_or_personne) {
       ==> Il suffit de le retirer, c'est du Bootstrap ; mais ça rapproche les inputs du coin haut-gauche du from-group, c'est moins beau ;) 
   */
 
-    if(isset($_GET["letter_nom"])) {  //  , $_GET["letter_prenom"] 
+    if(isset($_GET["letter_nom"])) {  //  || $_GET["letter_prenom"] --> error isset with expression ??? 
         $contents = html_form_search($acte_or_personne); 
     } else {
         $contents = html_form_letters($acte_or_personne);  
@@ -240,9 +184,6 @@ function html_tabpanel($class, $name, $acte_or_personne) {
         . $class . '" id="' . $name . '">
                 <section>
                     <div>' 
-                        .// afficher_forms($name, $acte_or_personne) . 
-                        //  html_form_search($acte_or_personne) . 
-                        // html_form_letters($acte_or_personne) . 
                         $contents . 
                     '</div>
                 </section>
@@ -264,29 +205,3 @@ echo html_tab_contents();
 
 ?>
 
-<!-- <ul class="nav nav-tabs" role="tablist">
-    <li role="presentation" class="active">
-        <a href="recherche#actes" aria-controls="actes" role="tab" data-toggle="tab">Actes</a>
-    </li>
-    <li role="presentation">
-        <a href="recherche#personnes" aria-controls="personnes" role="tab" data-toggle="tab">Personnes</a>
-    </li>
-</ul> -->
-
-<!-- <div class="tab-content">
-    <div role="tabpanel" class="tab-pane active" id="actes">
-        <section>
-            <?php 
-            //    echo html_form_search('acte');
-            ?>
-        </section>
-    </div>
-
-    <div role="tabpanel" class="tab-pane" id="personnes">
-        <section>
-            <?php 
-            //    echo html_form_search('personne');
-            ?>
-        </section>
-    </div>
-</div> -->
